ed8176929af17f25e2ad282c587c022470c5365e
lsaari <lsaari@kth.se>
2018-03-10 19:31:22 +0800

    UI uppdateringar så att det ser snyggare ut

--- rssapp/components/pages/News.js
+++ rssapp/components/pages/News.js
@@ -1,6 +1,6 @@
 // Template from https://react-native-training.github.io/react-native-elements/docs/0.19.0/lists.html#listitem-implemented-with-custom-view-for-subtitle
 import React, { Component } from 'react';
-import { ScrollView } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 import { List, ListItem } from 'react-native-elements';
 import { NavigationActions } from 'react-navigation';
 
@@ -15,8 +15,8 @@ class News extends Component {
     const{navigate} = this.props.navigation;
     articles = JSON.parse(this.props.navigation.state.params.news._bodyInit)["articles"]  // Array of articles stored in dicts
     return(
-      <ScrollView contentContainerStyle={{paddingVertical: 20}}>
-        <List containerStyle={{marginBottom: 20}}>
+      <ScrollView style={styles.scrollContainer}>
+        <List style={styles.listContainer}>
         {
           // Iterates over the articles and displays them
           articles.map((article, i) => (
@@ -26,6 +26,9 @@ class News extends Component {
               subtitle={"Publicerad: " + article.pubTime + "\nUpvotes: " + article.upvoteCount
                         + " Kommentarer: " + article.commentCount}
               subtitleNumberOfLines = {2}  // Subtitle is given two lines of space
+              titleStyle={{color: 'white'}}
+              subtitleStyle={{color: 'white'}}
+              containerStyle={{backgroundColor: '#2c3e50'}}
 
               // When the article is pressed, move the user to the article specific page and display the article
               onPress={() => navigate("Article", {title: article.title, content: article.content, id: article.id})}
@@ -37,5 +40,16 @@ class News extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  scrollContainer:{
+    paddingVertical: 20,
+    backgroundColor: '#2c3e50'
+  },
+  listContainer:{
+    //backgroundColor: '#2c3e50',
+    marginBottom: 20
+  }
+});
 
 export default News;
